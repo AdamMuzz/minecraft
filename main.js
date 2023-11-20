1129171c03dbc9cc9a56ca564c23bb7376e600ee
Sari Abu-Hamad <sari.abuhamad@gmail.com>
2023-11-20 15:29:53 -0800

    Update Control Panel + Movement Speed

--- main.js
+++ main.js
@@ -56,6 +56,7 @@ class CustomMovementControls extends defs.Movement_Controls {
 
         // Unfreeze mouse look around by default
         this.look_around_locked = false; // Set to false to allow looking around
+        this.speed_multiplier = 0.33;
     }
 
     // Override the display method to modify behavior
@@ -77,6 +78,32 @@ class CustomMovementControls extends defs.Movement_Controls {
 
         // Call the super class method
         super.first_person_flyaround(radians_per_frame, meters_per_frame, leeway);
+    }
+    make_control_panel(){
+        // Create the control panel elements you want to keep
+        this.control_panel.innerHTML += "Click and drag the scene to spin your viewpoint around it.<br>";
+        this.live_string(box => {
+            box.textContent = "- Position: " + this.pos[0].toFixed(2) + ", " + this.pos[1].toFixed(2)
+            + ", " + this.pos[2].toFixed(2)
+        });
+        this.new_line();
+
+        // Facing directions
+        this.live_string(box => {
+            box.textContent = "- Facing: " + ((this.z_axis[0] > 0 ? "West " : "East ")
+            + (this.z_axis[1] > 0 ? "Down " : "Up ") + (this.z_axis[2] > 0 ? "North" : "South"))
+        });
+        this.new_line();
+        this.new_line();
+
+        // Redefine the buttons you want to keep
+        this.key_triggered_button("Up", [" "], () => this.thrust[1] = -1, undefined, () => this.thrust[1] = 0);
+        this.key_triggered_button("Forward", ["w"], () => this.thrust[2] = 1, undefined, () => this.thrust[2] = 0);
+        this.new_line();
+        this.key_triggered_button("Left", ["a"], () => this.thrust[0] = 1, undefined, () => this.thrust[0] = 0);
+        this.key_triggered_button("Back", ["s"], () => this.thrust[2] = -1, undefined, () => this.thrust[2] = 0);
+        this.key_triggered_button("Right", ["d"], () => this.thrust[0] = -1, undefined, () => this.thrust[0] = 0);
+        this.new_line();
     }
 
 }
