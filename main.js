3fc05ae804bdc3f6fa7239c6a46abe5f4ba6f097
Adam Muzzarelli <admuzzarelli@gmail.com>
2023-11-20 16:02:47 -0800

    cleaned up code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {defs, tiny} from './examples/common.js';
 import { Chunk_Manager, Perlin_Noise } from './world_gen.js';
+import { CustomMovementControls } from './controls.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
@@ -46,62 +47,6 @@ class Cube extends Shape {
                 Square.insert_transformed_copy_into(this, [], square_transform);
             }
     }
-}
-
-class CustomMovementControls extends defs.Movement_Controls {
-    constructor() {
-        super();
-        // Disable rolling by default
-        this.roll = 0;
-
-        // Unfreeze mouse look around by default
-        this.look_around_locked = false; // Set to false to allow looking around
-        this.speed_multiplier = 0.4;
-    }
-
-    // Override the display method to modify behavior
-    display(context, graphics_state, dt = graphics_state.animation_delta_time / 1000) {
-        // Prevent rolling left or right
-        this.roll = 0;
-
-        // Call the super class display method
-        super.display(context, graphics_state, dt);
-    }
-
-    // Override the first_person_flyaround method to restrict up/down movement
-    first_person_flyaround(radians_per_frame, meters_per_frame, leeway = 70) {
-        // Prevent moving up and down
-        this.thrust[1] = 0;
-
-        this.mouse.from_center[1] = Math.max(Math.min(this.mouse.from_center[1], leeway/2), -leeway/2);
-
-
-        // Call the super class method
-        super.first_person_flyaround(radians_per_frame, meters_per_frame, leeway);
-    }
-    make_control_panel(){
-        this.control_panel.innerHTML += "Click and drag the scene to spin your viewpoint around it.<br>";
-        this.live_string(box => {
-            box.textContent = "- Position: " + this.pos[0].toFixed(2) + ", " + this.pos[1].toFixed(2)
-            + ", " + this.pos[2].toFixed(2)
-        });
-        this.new_line();
-        this.live_string(box => {
-            box.textContent = "- Facing: " + ((this.z_axis[0] > 0 ? "West " : "East ")
-            + (this.z_axis[1] > 0 ? "Down " : "Up ") + (this.z_axis[2] > 0 ? "North" : "South"))
-        });
-        this.new_line();
-        this.new_line();
-
-        this.key_triggered_button("Up", [" "], () => this.thrust[1] = -1, undefined, () => this.thrust[1] = 0);
-        this.key_triggered_button("Forward", ["w"], () => this.thrust[2] = 1, undefined, () => this.thrust[2] = 0);
-        this.new_line();
-        this.key_triggered_button("Left", ["a"], () => this.thrust[0] = 1, undefined, () => this.thrust[0] = 0);
-        this.key_triggered_button("Back", ["s"], () => this.thrust[2] = -1, undefined, () => this.thrust[2] = 0);
-        this.key_triggered_button("Right", ["d"], () => this.thrust[0] = -1, undefined, () => this.thrust[0] = 0);
-        this.new_line();
-    }
-
 }
 
 export class Main extends Scene {
@@ -153,58 +98,44 @@ export class Main extends Scene {
     }
 
     set_camera_above_block() {
-        // Find a suitable block's position
-        let blockPosition = this.find_starting_block_position();
-        let cameraHeightAboveBlock = 10; // Adjust as needed
+        let blockPosition = this.find_starting_block_position();    // Find a suitable block's position
+        let cameraHeightAboveBlock = 10;                            // Adjust as needed
     
-        // Return the calculated camera position
         return [blockPosition[0], blockPosition[1] + cameraHeightAboveBlock, blockPosition[2]];
     }
     
     find_starting_block_position() {
-         // Access Chunk 0,0
-        const chunk = this.Chunk_Manager.chunks.get("0,0");
+        const chunk = this.Chunk_Manager.chunks.get("0,0"); // Access Chunk 0,0
+        const baseIndex = 16368;                            // Base index for position (31, y, 31)
+        let highestY = -1;                                  // Initialize the highest block's y-coordinate
 
-        // Base index for position (31, y, 31)
-        const baseIndex = 16368;
-
-        // Initialize the highest block's y-coordinate
-        let highestY = -1;
-
-        // Iterate over y-coordinates from 0 to 15
-        for (let y = 0; y <= 15; y++) {
-            // Calculate the index in the blocks array
-            let index = baseIndex + y;
-
-            // Check if the block at this index is not null
-            if (chunk.blocks[index] !== null) {
-                // Update the highest y-coordinate
-                highestY = y;
+        for (let y = 0; y <= 15; y++) {                     // Iterate over y-coordinates from 0 to 15
+            if (chunk.blocks[baseIndex + y] !== null) {     // Check if the block at this index is not null
+                highestY = y;                               // Update the highest y-coordinate
             }
         }
 
-    // If no valid block is found, return a default position
-    if (highestY === -1) {
-        // Return a default position if needed
-        return [-31, 0, -31]; // Example default position
-    }
+        if (highestY === -1) {                              // If no valid block is found, return a default position
+            return [-31, 0, -31];                           // Return a default position if needed
+        }
 
-    // Return the coordinates of the highest block
-    return [-31, -highestY - 5, -31];
+        return [-31, -highestY - 5, -31];                   // Return the coordinates of the highest block
     }
-    
 
     display(context, program_state) {
         // grab gl pointer to handle sky color
         const gl = context.context;
 
-        // create initial camera & lights on first frame
+        // on first frame...
         if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new CustomMovementControls());
-            let [x_initial, y_initial, z_initial] = this.set_camera_above_block();
+            this.children.push(context.scratchpad.controls = new CustomMovementControls());     // movement controls
+            let [x_initial, y_initial, z_initial] = this.set_camera_above_block();              // set player pos
             program_state.set_camera(Mat4.translation(x_initial, y_initial, z_initial));
-            program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 1, 256);
-            program_state.lights = [new Light(vec4(0, 20, 0, 1), color(1, 1, 1, 1), 10000)];
+            program_state.projection_transform = Mat4.perspective(                              // set camera as perspective
+                Math.PI / 4, 
+                context.width / context.height, 
+                1, 256);
+            program_state.lights = [new Light(vec4(0, 20, 0, 1), color(1, 1, 1, 1), 10000)];    // create lights
         }
 
         let model_transform;
